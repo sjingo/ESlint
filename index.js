4a8616fad625d5febc2ba35cd75c1de81d3c4e63
STEFAN <sjingo@users.noreply.github.com>
2018-09-23 11:16:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+class API_Handler {
+    constructor(name, requestObject){
+        
+    }
+    super();
+
+}
+
+const a = 2;
+var a =3;
+
+if(a == 2){
+    x = 5
+
+}
---
+++ src/test.js
@@ -0,0 +1,9 @@
+var error = 'error'
+
+let ar = []
+ar = [1]
+
+const two = 2
+two = 3
+
+console.log(error,ar)
